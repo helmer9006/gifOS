d0847befa3a5f844d7627166ffdb9e9085600518
HELMER VILLARREAL <SISTEMAS@ucmagd.local>
2020-01-16 22:20:33 -0500

    Funcionalidad buscar lista

--- js/giphy.js
+++ js/giphy.js
@@ -20,7 +20,7 @@ export default class Giphy {
         try {
             
         
-             let api = await fetch('https://api.giphy.com/v1/gifs/search?api_key='+ this.apiKey+ '&q=' + this.search + '&limit=24&offset=0&rating=G&lang=es');
+            let api = await fetch('https://api.giphy.com/v1/gifs/search?api_key='+ this.apiKey+ '&q=' + this.search + '&limit=24&offset=0&rating=G&lang=es');
             let found = await api.json();
             return found;
 
@@ -64,10 +64,3 @@ export default class Giphy {
     //#endregion
 
 }
-
-
-// const resBusqueda = new Giphy("perro");
-// resBusqueda.getSearchResults()
-// .then( res => {
-//     console.log(res)
-// })
